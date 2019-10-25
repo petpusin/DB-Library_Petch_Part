fb09b7bdac61ed0427d959b5b8418fa10872c7a6
Kritsana Manupha <kritsana.mnp@gmail.com>
2019-10-25 15:53:04 +0700

    delete sub folder

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -37,8 +37,6 @@
 
                             <div class="col-md-6">
 
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
                                 <input id="password" type="pass" placeholder="PASSWORD" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
 

5f607e19aeea79bdcb7fb3bcc047d2edfebfacd9
Suphakorn Homnan <supakornhomnan@gmail.com>
2019-11-06 23:15:33 +0700

    add stock page

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 <!-- CSS-style -->
-
-
+@section('head')
+<link href="{{ asset('css/signin.css')}}" rel="stylesheet">
 <link rel="stylesheet" href="/css/stock.css">
+<!-- FROM GOOGLE FONT -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+@endsection
 
 @section('content')
 <div class="searchBox" align="center">
@@ -15,135 +18,360 @@
     </button>
 </div>
 <br><br>
-<div class="container">
-    <table style="width:100%">
-        <tr style="background-color:black; color:white">
-            <th style="text-align:center">OrderDate</th>
-            <th style="text-align:center">Amount</th>
-            <th style="text-align:center">ProductCode</th>
-            <th style="text-align:center">Name</th>
-            <th style="text-align:center">Edit</th>
-            <th style="text-align:center">Delete</th>
-        </tr>
-        <tr>
-            <td>27/10/2019</td>
-            <td>123</td>
-            <td>ABC</td>
-            <td>Nice</td>
 
-            <td>
-                <div class="box">
-                    <img src="/img/edit.png" alt="" class="center">
-                </div>
+<!-- make table resposive -->
+<div class="table-responsive">   
+    <div class="container">
+        <div class="row row-centered pos">
+            <div class="col-lg-8 col-centered">
+                <table class="table table-hover">
+                    <tr style="background-color:black; color:white" class="Headtable">
+                        <th>OrderDate</th>
+                        <th>Amount</th>
+                        <th>ProductCode</th>
+                        <th>Name</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
 
-            </td>
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
 
-
-            <td>
-                <div class="box2">
-                    <img src="/img/delete.png" alt="" class="center">
-                </div>
-
-            </td>
-        </tr>
-        <tr>
-            <td>27/10/2019</td>
-            <td>123</td>
-            <td>ABC</td>
-            <td>Nice</td>
-
-            <td>
-                <div class="box">
-                    <img src="/img/edit.png" alt="" class="center">
-                </div>
-
-            </td>
+                        </td>
 
 
-            <td>
-                <div class="box2">
-                    <img src="/img/delete.png" alt="" class="center">
-                </div>
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
 
-            </td>
-        </tr>
-        <tr>
-            <td>27/10/2019</td>
-            <td>123</td>
-            <td>ABC</td>
-            <td>Nice</td>
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
 
-            <td>
-                <div class="box">
-                    <img src="/img/edit.png" alt="" class="center">
-                </div>
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
 
-            </td>
-
-
-            <td>
-                <div class="box2">
-                    <img src="/img/delete.png" alt="" class="center">
-                </div>
-
-            </td>
-        </tr>
-        <tr>
-            <td>27/10/2019</td>
-            <td>123</td>
-            <td>ABC</td>
-            <td>Nice</td>
-
-            <td>
-                <div class="box">
-                    <img src="/img/edit.png" alt="" class="center">
-                </div>
-
-            </td>
+                        </td>
 
 
-            <td>
-                <div class="box2">
-                    <img src="/img/delete.png" alt="" class="center">
-                </div>
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
 
-            </td>
-        </tr>
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
 
-        <tr>
-            <td>OrderDate</td>
-            <td>Amount</td>
-            <td>ProductCode</td>
-            <td>Name</td>
-            <td style="color:red" colspan="2">Add</td>
-        </tr>
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
 
-        <tr>
-            <div class="topnav">
-                <td style="background-color:#ddd">
-                    <input type="text" placeholder="" class="general-search">
-                </td>
-                <td style="background-color:#ddd">
-                    <input type="text" placeholder="" class="general-search">
-                </td>
-                <td style="background-color:#ddd">
-                    <input type="text" placeholder="" class="general-search">
-                </td>
-                <td style="background-color:#ddd">
-                    <input type="text" placeholder="" class="general-search">
-                </td>
-                <td style="background-color:#ddd" colspan="2">
-                    <div class="box3">
-                        <img src="/img/add.png" alt="" class="center">
-                    </div>
-                </td>
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+                    <tr>
+                        <td>27/10/2019</td>
+                        <td>123</td>
+                        <td>ABC</td>
+                        <td>Nice</td>
+
+                        <td>
+                            <div class="box">
+                                <img src="/img/edit.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                        <td>
+                            <div class="box2">
+                                <img src="/img/delete.png" alt="" class="center">
+                            </div>
+
+                        </td>
+
+
+                    <tr>
+                        <td>OrderDate</td>
+                        <td>Amount</td>
+                        <td>ProductCode</td>
+                        <td>Name</td>
+                        <td style="color:red" colspan="2">Add</td>
+                    </tr>
+
+                    <tr>
+                        <div class="topnav">
+                            <td>
+                                <input type="text" placeholder="" class="general-search">
+                            </td>
+                            <td>
+                                <input type="text" placeholder="" class="general-search">
+                            </td>
+                            <td>
+                                <input type="text" placeholder="" class="general-search">
+                            </td>
+                            <td>
+                                <input type="text" placeholder="" class="general-search">
+                            </td>
+                            <td colspan="2">
+                                <div class="box3">
+                                    <img src="/img/add.png" alt="" class="center">
+                                </div>
+                            </td>
+                        </div>
+
+
+                    </tr>
+                </table>
             </div>
 
 
-        </tr>
-    </table>
-
-
+        </div>
+    </div>
 </div>
 
 
@@ -151,5 +379,5 @@
 
 
 
-<script src="resources/js/searchengine.js"></script>
+<!-- <script src="resources/js/searchengine.js"></script> -->
 @endsection

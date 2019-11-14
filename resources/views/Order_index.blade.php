bd4104aa7820e3971ec3c3fa2f19a2dbd82ca7de
Kritsana Manupha <kritsana.mnp@gmail.com>
2019-11-14 17:07:40 +0700

    Update Ui order page

--- resources/views/Order_index.blade.php
+++ resources/views/Order_index.blade.php
@@ -3,12 +3,6 @@
 @section('head')
 
   <link href="{{ asset('css/signin.css')}}" rel="stylesheet">
-  
-  <style>
-    table,th,td {
-      border: 1px solid black;    
-    }
-  </style>
 
 @endsection
 
@@ -20,15 +14,35 @@
 
 @section('content')
 
+  <style>
+    tr:nth-child(odd) {
+    background-color: #f2f2f2;
+    }
+
+    footer {
+    font-size: 10px;
+    font-weight: lighter;
+    color: white;
+    text-align: center;
+    position: relative;
+    margin-top: 40px;
+    margin-bottom: 10px;
+    }
+
+    hr{
+      background-color:white;
+      height:0.5px;
+    }
+  </style>
   <div class="container">
   
     <h1 class="display-4 text-center mb-4 text-light">ORDER</h1>
-    <h3 class="display-9 text-light">Search Results : <a id="total"></a></h3> <!-- SEARCH COUNT ROW MATCH -->
+    <h3 class="display-9 text-light">Order Results : <a id="total"></a></h3> <!-- SEARCH COUNT ROW MATCH -->
     <div class="row">
       <div class="col-md-12" > <!-- BECAUSE 4*3=12 -->
         
         <div class="card my-4" style="background-color:black;opacity:0.9"> <!-- Search Widget -->
-          <h5 class="card-header"><img src="/img/magnifier.svg" width="30px" alt=""> : &nbsp;<div class="btn" style="background-color:#00313C;color:white;" id="show_">OrderNumber</div></h5>
+          <h5 class="card-header"><img src="/img/magnifier.svg" width="25px" alt=""> Title : &nbsp;<div class="btn" style="background-color:#00313C;color:white;" id="show_">OrderNumber</div></h5>
           
           <div id="search">
           <div class="select text-center my-2">
@@ -44,65 +58,65 @@
           
           <div class="card-body">
             <div class="input-group">
-              <input type="text" id="input" onkeyup="search()" class="form-control" placeholder="Search for...">
+              <input type="text" id="input" onkeyup="search()" class="form-control" placeholder="Search value ...">
             </div>
           </div>
 
         </div>
       </div>
         
-      <div class="card my-4 bg-light text-dark ">
+      <div class="bg-light">
         <table id="myTR">
-          <tr style="background-color:black;color:white">
+          <tr style="background-color:#3b444b;color:white">
             <th height="70px">
                 <center>
-                  <font size="2px">Order No.</font>
+                  <font size="2px" class="font-weight-normal">Order No.</font>
                 </center>
             </th>
             <th width="10%">
                 <center>
-                  <font size="2px">Order Date</font>
+                  <font size="2px" class="font-weight-normal">Order Date</font>
                 </center>
             </th>
             <th>
                 <center>
-                  <font size="2px">Required Date</font>
+                  <font size="2px" class="font-weight-normal">Required Date</font>
                 </center>
             </th>
             <th>
                 <center>
-                  <font size="2px">Shipped Date</font>
+                  <font size="2px" class="font-weight-normal">Shipped Date</font>
                 </center>
             </th>
             <th width="10%">
                 <center>
-                  <font size="2px">Status</font>
+                  <font size="2px" class="font-weight-normal">Status</font>
                 </center>
             </th>
             <th width="35%">
                 <center>
-                  <font size="2px">Comments</font>
+                  <font size="2px" class="font-weight-normal">Comments</font>
                 </center>
             </th>
             <th width="10%">
               <center>
-                <font size="2px">First Name</font>
+                <font size="2px" class="font-weight-normal">First Name</font>
               </center>
             </th>
             <th width="10%">
               <center>
-                <font size="2px">Last Name</font>
+                <font size="2px" class="font-weight-normal">Last Name</font>
               </center>
             </th>
             <th width="5%">
               <center>
-                <font size="2px">Edit</font>
+                <font size="2px" class="font-weight-normal">Edit</font>
               </center>
             </th>
           </tr>
 
           @foreach($a as $aa)
-          <tr>
+          <tr style="height:70px">
             <td style="font-size:13px;">
               <center>{{ $aa->orderNumber }}</center>
             </td>
@@ -115,23 +129,24 @@
             <td style="font-size:13px;" >
               <center>{{ $aa->shippedDate }}</center>
             </td>
-            <td style="font-size:14px; color:red">
+            <td style="font-size:14px; color:red;font-weight:lighter;">
               <center>{{ $aa->status }}</center>
             </td>
             <td style="font-size:12px;font-weight:lighter;">
               {{ $aa->comments }}
+              
             </td>
 
             @foreach($b as $bb)
             @if( $bb->customerNumber == $aa->customerNumber )
-            <td style="font-size:12px;font-weight:bold;">
+            <td style="font-size:12px;font-weight:normal;">
               <center>{{ $bb->contactFirstName }}</center>
             </td>
-            <td style="font-size:12px;font-weight:bold;">
+            <td style="font-size:12px;font-weight:normal;">
               <center>{{ $bb->contactLastName }}</center>
             </td>
             <td>
-              <center><a type="" href="/admin/order/edit/{{$aa->orderNumber}}"><img src="/img/pencil.svg" width="35" height="35" alt="edit"></a></center>
+              <center><a type="" href="/admin/order/edit/{{$aa->orderNumber}}"><img src="/img/pencil.svg" width="30px" height="30px" alt="edit"></a></center>
             </td>
             @endif
             @endforeach
@@ -144,6 +159,12 @@
     
   </div>
 
+  <footer>      <!-- FOR CONTACT -->
+  <hr>
+  <br>
+      <img src="/img/phone-book.svg" width="18px"; height="18px"> CONTACT US<br>cpeg2DB@GMAIL.COM | TEL XXX-XXXXXXX
+    </footer>
+    
 @endsection
 
 @section('script')
--- resources/views/create/product.blade.php
+++ resources/views/create/product.blade.php
@@ -26,12 +26,12 @@
 
     <div class="col-7">
         <div class="text-center rounded-pill py-1" style="color:#FFD400 ; background-color:black">NAME OF PRODUCT</div>
-        <hr style="border:solid 1px black">
-        <font>Description</font>
-        <p class=" text-break ml-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <hr style="background-color:black">
+        <font style="font-size:20px">Description</font><br><font style="font-size:16px">Scale 1:50</font><br><br>
+        <p class="text-break ml-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 
-        </p>
-        <hr style="border:solid 1px black">
+        </p><br>
+        <hr style="background-color:black">
         <div class="row mx-auto justify-content-center">
             <div class="col-3"><button type="button" class="mt-4 p-2" style="background-color:black ; border:none ; color:#FFD400 ; font-size:14px">ADD TO CART</button></div>
         </div>
